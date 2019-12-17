5f41e86652b3dc22dc0a56e80e3e84fa69e2a2c2
yitzman <38329004+yitzman@users.noreply.github.com>
2019-12-17 20:29:12 +0200

    new project

---
+++ cmake-build-debug/CMakeFiles/EX3.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/user/Documents/GitHub/EX3/Command.cpp" "C:/Users/user/Documents/GitHub/EX3/cmake-build-debug/CMakeFiles/EX3.dir/Command.cpp.obj"
+  "C:/Users/user/Documents/GitHub/EX3/main.cpp" "C:/Users/user/Documents/GitHub/EX3/cmake-build-debug/CMakeFiles/EX3.dir/main.cpp.obj"
+  "C:/Users/user/Documents/GitHub/EX3/openServerCommand.cpp" "C:/Users/user/Documents/GitHub/EX3/cmake-build-debug/CMakeFiles/EX3.dir/openServerCommand.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/EX3.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EX3.dir/Command.cpp.obj"
+  "CMakeFiles/EX3.dir/main.cpp.obj"
+  "CMakeFiles/EX3.dir/openServerCommand.cpp.obj"
+  "EX3.exe"
+  "EX3.exe.manifest"
+  "EX3.pdb"
+  "libEX3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EX3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
